3fcf119f6cfb707c481c3d81b151052d379c578f
Phu Nguyen Huu <cauchupgStudy@gmail.com>
2017-09-18 22:50:26 +0700

    pymicdgemm

--- mul_performance.py
+++ mul_performance.py
@@ -23,14 +23,35 @@ offl_c = stream.bind(c)
 
 pymic_dot_start = time.time()
 offl_c = offl_a * offl_b
-offl_c.update_host()
 stream.sync()
 pymic_dot_end = time.time()
 
 print("pymic dot :")
 print("--------------------------------------")
+offl_c.update_host()
 print(offl_c)
 print("checksum:", np.sum(offl_c.array))
 print("Run time:", pymic_dot_end - pymic_dot_start)
+print()
+print("--------------------------------------")
+
+c[:] = 0.0
+offl_c.update_device()
+alpha = 1.0
+beta = 0.0
+library = device.load_library("libdgemm.so")
+pymic_dgemm_start = time.time()
+stream.invoke(library.dgemm_kernel,
+              offl_a, offl_b, offl_c,
+              m, n, k, alpha, beta)
+stream.sync()
+pymic_dgemm_end = time.time()
+
+print("pymic dgemm :")
+print("--------------------------------------")
+offl_c.update_host()
+print(offl_c)
+print("checksum:", np.sum(offl_c.array))
+print("Run time:", pymic_dgemm_end - pymic_dgemm_start)
 print()
 print("--------------------------------------")
