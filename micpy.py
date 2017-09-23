642b829c43f0cf0278b12b670e94aad4c349a0eb
Phu Nguyen Huu <cauchupgStudy@gmail.com>
2017-09-23 13:04:48 +0700

    bugifxsum

--- micpy.py
+++ micpy.py
@@ -4,16 +4,18 @@ import time
 import sys
 
 def sum(operand1, operand2, axis=None, dtype=None) :
-	if operand1.dtype != operand1.astype(np.float32) || operand2.dtype != operand2.astype(np.float32):
-		return -1
+	#if operand1.dtype != operand1.astype(np.float32) or operand2.dtype != operand2.astype(np.float32):
+	#	return -1
 	device = mic.devices[0]
-	#libray = device.load_library("sumtest.so")
+	dt = map_data_types(operand1.dtype)
+	n = operand1.size
+	library = device.load_library("libmic.so")
 	stream = device.get_default_stream()
-	result = 0
+	result = np.zeros((m, n))
 	offl_o1 = stream.bind(operand1)
 	offl_o2 = stream.bind(operand2)	
 	offl_r = stream.bind(result)
-	stream_invoke (library.sumkernel, offl_o1, offl_o2, offl_r)
+	stream_invoke (library.pymic_offload_array_sum, dt, n, offl_o1, offl_r)
 	offl_r.update_host()
 	stream.sync()
 	print (result)
